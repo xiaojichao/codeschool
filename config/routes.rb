05dabcd67592f9733b0ca4247b2255f5a7d69db0
Sergey Chipiga <sergey.chipiga@flatstack.com>
2012-10-12 20:11:00 +0400

    fix root_path error

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 CodeSchool::Application.routes.draw do
-  devise_for :users do
-    root to: "devise/sessions#new"
-  end
+  devise_for :users
 
   namespace 'admin' do
     resources :courses do
       resources :challenges
     end
     resources :users
-     root to: 'courses#index'
   end
 
  resources :courses, only: [:index, :show]
@@ -20,4 +17,6 @@ CodeSchool::Application.routes.draw do
   authenticated :user do
     root to: 'courses#index'
   end
+
+  root to: 'courses#index'
 end
