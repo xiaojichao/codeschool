6273211986d4c3f6205f7e9267c6f9a98a8a386d
Vladimir Mikhailov <vladimir.mikhailov@flatsourcing.com>
2012-10-12 18:17:06 +0400

    added authenticate_user before_filter

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 require 'rdoc'
 
 class DashboardController < ApplicationController
+  skip_before_filter :authenticate_user!
+
   def index
     @rdoc = RDoc::Markup::ToHtml.new.convert(File.read(File.join(Rails.root, 'doc', 'README_FOR_APP_TEMPLATE.rdoc')))
   end
