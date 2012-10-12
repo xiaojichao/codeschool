2bb7fa97d46258b7acffab0d6b67697d8cf74629
Vladimir Mikhailov <vladimir.mikhailov@flatsourcing.com>
2012-10-12 19:32:39 +0400

    Use sendgrid for emails on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,13 +65,12 @@ CodeSchool::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  # Heroku specific setting for Cedar stack http://devcenter.heroku.com/articles/sendgrid#usage
-  # config.action_mailer.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com'
-  # }
+  config.action_mailer.smtp_settings = {
+   :address        => 'smtp.sendgrid.net',
+   :port           => '587',
+   :authentication => :plain,
+   :user_name      => ENV['SENDGRID_USERNAME'],
+   :password       => ENV['SENDGRID_PASSWORD'],
+   :domain         => 'heroku.com'
+  }
 end
